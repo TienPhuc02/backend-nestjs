9545fc0a50408dea156962e83a39a1bab8ad0be0
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-08-29 14:37:45 +0700

    mongoDB with nestjs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,13 +4,15 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  @Get() /// => api(restful)
-  @Render('home') //SSR render hone.ejs
+  @Get() 
+  @Render('home') 
   getHello() {
     const message = this.appService.getHello();
     return {
       message: message,
     };
-    // return 'this.appService.getHello()';
   }
 }
+
+//database select * from user => raw query
+// ORM/ODM =>find()
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://dtp2352002:jt0pqKrSRn6F6kKG@cluster2.hchoqcj.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
