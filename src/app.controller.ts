89c354e2e8ca121bb8a212dac677d6f5b29dfc8e
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-08-29 08:31:57 +0700

    TS decorators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller() //decorators -> syntax @function()-> nói cho trình compile biết là kí hiệu,...
+//@expression -> expresion là diễn tả nó làm gì
+
+//class User {
+// @Min(0)
+// @Max(10)
+// @IsEmail
+// email: string;
+// @Max(20)
+// password: string;
+
+// }
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
     return this.appService.getHello();
