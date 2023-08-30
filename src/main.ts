00cef171f97881ae5b548818908cbd312be9577d
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-08-30 10:58:54 +0700

    Validation

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   app.useStaticAssets(join(__dirname, '..', 'public')); //js ,css,image
   app.setBaseViewsDir(join(__dirname, '..', 'views')); //store view engine
   app.setViewEngine('ejs');
+  app.useGlobalPipes(new ValidationPipe());
   const port = configService.get<string>('PORT');
   await app.listen(port);
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,22 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
 export class CreateUserDto {
+  @IsEmail({}, { message: 'Invalid email message' })
+  @IsNotEmpty({
+    message: 'Please Enter Your Email',
+  })
   email: string;
+  @IsNotEmpty({
+    message: 'Please Enter Your Password',
+  })
   password: string;
+  @IsNotEmpty({
+    message: 'Please Enter Your Name',
+  })
   name: string;
-  addressL: string;
+  @IsNotEmpty({
+    message: 'Please Enter Your Address',
+  })
+  address: string;
 }
 //DTO , muốn data giữa FE và BE thống nhất với nhau
 //không dùng interface cho DTO -> dùng class
